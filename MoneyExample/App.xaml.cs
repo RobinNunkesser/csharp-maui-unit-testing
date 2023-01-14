15f2d620c0200766e3f891df7f1404bf821930e2
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-01-14 23:37:26 +0100

    Initial commit

---
+++ MoneyExample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyExample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MoneyExample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoneyExample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
